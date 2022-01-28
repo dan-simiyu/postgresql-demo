5910d7844aa4518b303e3e7b93cf174e30112254
dan-simiyu <63334852+dan-simiyu@users.noreply.github.com>
2022-01-28 12:13:17 +0300

    initial commit

---
+++ src/main/java/com/devsim/postgresqldemo/entities/Teacher.java
@@ -0,0 +1,17 @@
+package com.devsim.postgresqldemo.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Teacher  extends EntityWithUUID{
+    private String name;
+    private String pictureURL;
+    private String email;
+}
